27b072977adadd9050518e3fb6a9564114df9b02
Qwester471 <80134273+Qwester471@users.noreply.github.com>
2025-05-14 04:33:10 +0300

    Add files via upload

---
+++ KnowledgeApp/KnowledgeApp.DataAccess/Database/Entities/FacultyStatistic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KnowledgeApp.DataAccess.Entities;
+
+public partial class FacultyStatistic
+{
+    public int Id { get; set; }
+    public string FacultyName { get; set; } = null!;
+    public int? GroupCountMust { get; set; }
+    public int? GroupCountFact { get; set; }
+    public int? DisciplineCountMust { get; set; }
+    public int? DisciplineCountFact {  get; set; }
+    public int? EReportDoneCount { get; set; }
+    public int? EReportRevCount { get; set; }
+    public int? PapReportDoneCount { get; set; }
+    public int? PapReportRevCount { get; set; }
+    public bool? AllDone { get; set; }
+    public Faculty Faculty { get; set; }
+}
