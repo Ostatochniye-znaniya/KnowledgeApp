106cc645dd2217fa15521d3acdd7176c7393ad94
Арсений <73458752+she1kopr9d@users.noreply.github.com>
2026-03-25 17:10:10 +0300

    Fix init.sql

--- init.sql
+++ init.sql
@@ -1,5 +1,4 @@
 -- init.sql
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'admin';
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
