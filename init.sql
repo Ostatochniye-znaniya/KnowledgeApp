65b94f09c8beaf0759de649bcdcb99ec0e931127
Арсений <73458752+she1kopr9d@users.noreply.github.com>
2026-03-25 17:29:42 +0300

    Delete init.sql

--- init.sql
+++
@@ -1,8 +0,0 @@
--- init.sql
--- Удаляем существующую запись, если есть
-DELETE FROM mysql.user WHERE User='root' AND Host='%';
--- Добавляем новую
-CREATE USER 'root'@'%' IDENTIFIED BY 'admin';
--- Даём права
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
