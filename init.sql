3dbb5127ffba4028eec2c0aeb028f5545c786b8e
Арсений <73458752+she1kopr9d@users.noreply.github.com>
2026-03-25 17:27:09 +0300

    Fix init.sql

--- init.sql
+++ init.sql
@@ -1,2 +1,8 @@
-UPDATE mysql.user SET HOST='%' WHERE User='root';
+-- init.sql
+-- Удаляем существующую запись, если есть
+DELETE FROM mysql.user WHERE User='root' AND Host='%';
+-- Добавляем новую
+CREATE USER 'root'@'%' IDENTIFIED BY 'admin';
+-- Даём права
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
